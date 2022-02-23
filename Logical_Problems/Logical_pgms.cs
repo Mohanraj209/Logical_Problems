8935d49efd4fb603c0c6e4bb0e8f5555b4440df9
Mohanraj209 <mohanraj1715@gmail.com>
2022-02-24 00:11:27 +0530

    Program to print an Perfect number

--- Logical_Problems/Logical_pgms.cs
+++ Logical_Problems/Logical_pgms.cs
@@ -17,15 +17,42 @@ namespace Logical_Problems
             Console.Write("\n Enter the number of elements: ");
             number = int.Parse(Console.ReadLine());
             Console.Write(n1 + " " + n2 + " "); //printing 0 and 1    
-            for (i = 2; i < number; ++i) //loop starts from 2 because 0 and 1 are already printed    
+            for (i = 2; i < number; i++) //loop starts from 2 because 0 and 1 are already printed    
             {
                 n3 = n1 + n2;
-                Console.Write(n3 + " ");
+                Console.Write( n3 + " ");
                 n1 = n2;
                 n2 = n3;
             }
         }
-    }
+
+		/// <summary>
+		/// Code for Perfect number..
+		/// </summary>
+		public static void Perfect_num()
+		{
+
+			int i, num, Sum = 0;
+			Console.Write("\n\n Please Enter any Number: ");
+			num = Convert.ToInt32(Console.ReadLine());
+
+			for (i = 1; i < num; i++)
+			{
+				if (num % i == 0)
+				{
+					Sum = Sum + i;
+				}
+			}
+			if (Sum == num)
+			{
+				Console.WriteLine(String.Format("{0} is a Perfect Number", num));
+			}
+			else
+			{
+				Console.WriteLine(String.Format("{0} is NOT a Perfect Number", num));
+			}
+		}
+	}
 }
 
 
--- Logical_Problems/Program.cs
+++ Logical_Problems/Program.cs
@@ -12,6 +12,7 @@ namespace Logical_Problems
         {
             Console.WriteLine("--------**** Welcome to Logical Programs ****-------");
             Logical_pgms.Fibonacci();
+            Logical_pgms.Perfect_num();
         }
     }
 }
